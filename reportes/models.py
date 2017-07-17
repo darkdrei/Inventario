af83055fe55907b0a084b3e192ef915c73deec5d
darkdrei <darkdrei88@gmail.com>
2017-07-16 21:49:10 -0500

    modulo de reportes

---
+++ reportes/models.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+class UnidadNedocio(models.Model):
+    inicio = models.DateField(auto_now=True, auto_now_add=True)
+    fin = models.DateField(auto_now=True, auto_now_add=True)
+
+    def __unicode__(self):
+        i = 0
+        men = ''
+        while i < 10 - len(str(self.instance.pk)):
+            men = men + '0'
+            i = i+1
+        # end ford
+        t = '%s%d' % (str(men), self.instance.pk)
+        print t
+        return u'%s' % (t)
+    #end def
+
+    def __str__(self):
+        i = 0
+        men = ''
+        while i < 10 - len(str(self.instance.pk)):
+            men = men + '0'
+            i = i+1
+        # end ford
+        t = '%s%d' % (str(men), self.instance.pk)
+        print t
+        return u'%s' % (t)
+    #end def
+
+    class Meta:
+        verbose_name = "Reporte unidad de negocio"
+        verbose_name = "Reporte de unidades de negocios"
+    #end class
