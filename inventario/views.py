97bdb357815d5360e69bef4b1072a9cd51ef8ac8
darkdrei <darkdrei88@gmail.com>
2017-07-16 21:48:24 -0500

    agregar el servicio de mostrar activos por el id del proveedor

--- inventario/views.py
+++ inventario/views.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from supra import views as supra
+import models
 
 # Create your views here.
+
+class ListArtProveedor(supra.SupraListView):
+    model = models.Activo
+    list_display = ['id', 'nombre', 'codigo']
+    search_key = 'q'
+    list_filter = ['proveedor__id']
+    search_fields = ['proveedor__id']
+
+    def get_queryset(self):
+        queryset = super(ListArtProveedor, self).get_queryset()
+        obj = queryset.order_by('nombre')
+        return obj
+    # end def
+# end class
